8284df14c42b550229dc304a79ee8ce0458f911b
yashkuncolienker-vb <89966303+yashkuncolienker-vb@users.noreply.github.com>
2021-09-29 19:56:17 +0530

    000|YASH RK|Shopping cart Icon added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import './Navbar.css';
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const cart = useSelector((state) => state.cart);
@@ -7,6 +9,13 @@ const Navbar = () => {
     <nav>
       <h1>Dashboard</h1>
       <h2>Cart - {cart.count}</h2>
+      <button>
+        <FontAwesomeIcon
+          size="2x"
+          className="cart-icon"
+          icon={faShoppingCart}
+        />
+      </button>
     </nav>
   );
 };
